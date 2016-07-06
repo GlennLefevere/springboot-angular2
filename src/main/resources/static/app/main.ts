7f7cf56fb0c1f20f097bb638d935dda62f09c944
Glenn Lefevere <Glenn Lefevere@NB-INF-0138.admbnet.be>
2016-07-06 15:43:12 +0200

    Enable prod mode and changed some style

--- src/main/resources/static/app/main.ts
+++ src/main/resources/static/app/main.ts
@@ -12,6 +12,9 @@ import { provideForms, disableDeprecatedForms } from '@angular/forms';
 import { AppComponent }         from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
+import {enableProdMode} from '@angular/core';
+enableProdMode();
+
 /*
 bootstrap(AppComponent, [
   APP_ROUTER_PROVIDERS,
